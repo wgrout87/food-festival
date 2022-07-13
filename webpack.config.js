62dafac4f7a9761e0bdc09fb082ff7c85b3c2518
Will G <wgrout87@gmail.com>
2022-07-13 17:52:10 -0600

    pwa manifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const path = require('path');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
     entry: {
@@ -44,6 +45,22 @@ module.exports = {
         }),
         new BundleAnalyzerPlugin({
             analyzerMode: 'static',
+            openAnalyzer: false
+        }),
+        new WebpackPwaManifest({
+            name: 'Food Event',
+            short_name: 'Foodies',
+            description: 'An app that allows you to view upcoming food events.',
+            start_url: '../index.html',
+            background_color: "#01579b",
+            theme_color: '#ffffff',
+            fingerprints: false,
+            inject: false,
+            icons: [{
+                src: path.resolve('assets/img/icons/icon-512x512.png'),
+                sizes: [96, 128, 192, 256, 384, 512],
+                destination: path.join('assets', 'icons')
+            }]
         })
     ],
     mode: 'development',
