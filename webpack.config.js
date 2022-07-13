f32fe46a2fd989f631c02d352b2b7ceebcd2ed6e
Will G <wgrout87@gmail.com>
2022-07-12 18:41:17 -0600

    optimized img files

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,50 @@
 const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const path = require('path');
 
 module.exports = {
-    entry: './assets/js/script.js',
+    entry: {
+        app: './assets/js/script.js',
+        events: './assets/js/event.js',
+        schedule: './assets/js/schedule.js',
+        tickets: './assets/js/ticket.js'
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name(file) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function (url) {
+                                return url.replace("../", "/assets/")
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
     },
     plugins: [
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery'
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static',
+        })
     ],
     mode: 'development'
 };
